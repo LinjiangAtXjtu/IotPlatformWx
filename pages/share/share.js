38e7d691f3dd05bff7b4ccf84bbaae0a88da776e
linjiang <595266161@qq.com>
2018-11-22 17:55:11 +0800

    图片展示d1

--- pages/share/share.js
+++ pages/share/share.js
@@ -1,12 +1,15 @@
 //share.js
 Page({
+  data:{
+    items: []
+  },
   //事件处理函数
   uploadPictures: function () {
     wx.chooseImage({
       success(res) {
         const tempFilePaths = res.tempFilePaths
         wx.uploadFile({
-          url: 'http://192.168.99.147:5000/pictureUpload',
+          url: 'http://192.168.99.147:5000/shareUpload',
           filePath: tempFilePaths[0],
           name: 'file',
           success(res) {
@@ -22,4 +25,20 @@ Page({
       }
     })
   },
-})
+  onload: function(){
+    var that = this;
+    wx.request({
+      url: 'http://192.168.99.147:5000/shareShow',
+      success(res) {
+        var list = that.data.items;
+        for (var i = 0; i < res.data.items.length; i++) {
+          list.push(res.data.items[i]);
+        }
+        that.setData({
+          items: list
+        });
+      }
+    })
+  },
+}
+)
